28ec0bd145e65e4970b58bb639aa3843ae706530
Pollacco <andrey.sviontek@gmail.com>
2019-10-31 11:23:40 +0500

    add to github

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+import Section from './Second.svelte';
+const elem = document.querySelector('.square');
+let color;
+
+function colorPicker(event) {
+	color = event.target.value;
+	console.log(color);
+	elem.style.backgroundColor = color;
+}
+
+</script>
+
+<style>
+
+div{
+	width: 100px;
+	height: 100px;
+	background: purple;
+}
+input{
+	margin-right: 20px;
+	width: 30px;
+	height: 30px;
+	border-radius: 50%;
+	}
+</style>
+
+<div class="square"></div>
+<h1>Choose color</h1>
+<input on:input={colorPicker} type="color" value={color}>
+
+<Section/>
---
+++ src/Second.svelte
@@ -0,0 +1,39 @@
+<script>
+// Array.from(document.querySelector('section').children).forEach(elem => {
+//     elem.addEventListener('click', e => {
+//         e.target.classList.toggle('.active');
+//         console.log('click', event);
+//     });
+// });
+</script>
+
+<style>
+.red{
+    width: 300px;
+    height: 100px;
+    background: red;
+}
+.blue{
+    width: 300px;
+    height: 100px;
+    background: blue;
+}
+.green{
+    width: 300px;
+    height: 100px;
+    background: green;
+}
+
+.active{
+    visibility: hidden;
+}
+</style>
+
+<section>
+    <div class="red active">
+    </div>
+    <div class="blue">
+    </div>
+    <div class="green">
+    </div>
+</section>
